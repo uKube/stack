04acf46cbfcd84ae1fdd42b655798843fd66bac5
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-11-07 02:44:35 +0100

    Enhance module
    Provide more feedback to the final user

--- modules/kubernetes-dashboard/dashboard.lan.sh
+++ modules/kubernetes-dashboard/dashboard.lan.sh
@@ -7,6 +7,18 @@ function sedeasy {
   sed -i "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
 }
 
+# Wait until POD is Running
+function wait_until_pod_is_running {
+  # Wait until the docker is up and running
+  echo -n ">> Waiting for '$1' to start..."
+  while [ ! $(kubectl get pods --all-namespaces -l $2 -o jsonpath="{.items[0].status.phase}" &>/dev/null && echo $?) ]
+  do
+      echo -n "."
+      sleep 0.5
+  done
+  echo "started!"
+}
+
 # Install helm dependency with configuration
 # $1: channel ( stable/incubator/test)
 # $2: app name
@@ -23,3 +35,11 @@ function helm_install_with_config {
 }
 
 helm_install_with_config "stable" "kubernetes-dashboard"
+
+wait_until_pod_is_running "kubernetes-dashboard" "app=kubernetes-dashboard"
+
+# Print friendly done message
+echo "-----------------------------------------------------"
+echo "All right! Everything seems to be installed correctly. Have a nice day!"
+echo ">> URL: http://${DOMAIN}/"
+echo "-----------------------------------------------------"
