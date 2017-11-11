c0c2e95aff2d020fdb2ef7b531d2b157ea647c71
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-11-11 20:46:17 +0100

    Purge CNI binaries too

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,9 @@
 # Purge the whole stack
 kubeadm reset &>/dev/null
 
+# Purge CNI binaries
+rm -Rf /opt/cni
+
 # Print friendly done message
 echo "--------------------------------------------"
 echo "Stack purged successfully. Have a nice day!"
