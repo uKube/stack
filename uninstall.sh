71aef1abff4172eb94efa9b9c32575225d7a7d9f
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-11-12 00:23:56 +0100

    Improve uninstall script
    Remove unnecessary files

--- uninstall.sh
+++ uninstall.sh
@@ -6,7 +6,27 @@ kubeadm reset &>/dev/null
 # Purge CNI binaries
 rm -Rf /opt/cni
 
+# Purge Kubernetes configurations
+rm -Rf /etc/kubernetes
+
+# Purge Kubernetes local files
+rm -Rf $HOME/.kube
+
+# Purge Helm local files
+rm -Rf $HOME/.helm
+
+# Disable the Kubelet service
+systemctl disable kubelet.service &>/dev/null
+systemctl stop kubelet.service &>/dev/null
+
+# Remove leftover files
+rm -f /etc/systemd/system/kubelet.service.d/99-local-extras.conf
+
+# Reload systemd
+systemctl daemon-reload &>/dev/null
+
 # Print friendly done message
-echo "--------------------------------------------"
-echo "Stack purged successfully. Have a nice day!"
-echo "--------------------------------------------"
+echo "-------------------------------------------------------------"
+echo "Stack purged successfully. It is suggested to reboot your VPS"
+echo "if you intend to reinstall again this stack. Have a nice day!"
+echo "-------------------------------------------------------------"
