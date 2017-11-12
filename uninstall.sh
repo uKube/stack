33630a366fb6f90f486befcae3ea9c526b8e5e66
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-11-12 17:31:51 +0100

    Improve uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -19,8 +19,15 @@ rm -Rf $HOME/.helm
 systemctl disable kubelet.service &>/dev/null
 systemctl stop kubelet.service &>/dev/null
 
+# Remove unneeded packages
+PACKAGES="ebtables ethtool socat docker kubeadm-bin kubectl-bin kubelet-bin kubernetes-helm-bin"
+yes '' | pacman -Rs --noconfirm $PACKAGES &>/dev/null
+
 # Remove leftover files
 rm -f /etc/systemd/system/kubelet.service.d/99-local-extras.conf
+rm -Rf /var/lib/kubelet
+rm -Rf /var/lib/etcd
+rm -Rf /var/lib/weave
 
 # Reload systemd
 systemctl daemon-reload &>/dev/null
